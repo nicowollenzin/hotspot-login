d8b9a6095b8620144a8bb17e34039a35f9dcc360
Nico Wollenzin <nico@wollenzin.de>
2014-04-30 23:09:04 +0200

    initial commit

---
+++ HotspotLogin/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uname = new System.Windows.Forms.TextBox();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login to Hotspot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "empty";
+            // 
+            // uname
+            // 
+            this.uname.Location = new System.Drawing.Point(85, 11);
+            this.uname.Name = "uname";
+            this.uname.Size = new System.Drawing.Size(155, 20);
+            this.uname.TabIndex = 2;
+            // 
+            // pwd
+            // 
+            this.pwd.Location = new System.Drawing.Point(85, 37);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(155, 20);
+            this.pwd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save Credentials";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(351, 110);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(37, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "author";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(353, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 127);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pwd);
+            this.Controls.Add(this.uname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "pp - Hotspot Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox uname;
+        private System.Windows.Forms.TextBox pwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HotspotLogin/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Web;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        string username = "";
+        string password = "";
+
+        Assembly _assembly;
+        Stream _imageStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try {
+            TextReader tr = new StreamReader("settings.cnf");
+            String passline = tr.ReadLine();
+            //if ()
+            string[] words = passline.Split('|');
+            username = words[0];
+            password = words[1];
+            tr.Close();
+            uname.Text = username;
+            pwd.Text = password;
+            } catch (FileNotFoundException) {
+                
+            }
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> postParameters = new Dictionary<string, string>();
+            HttpStatusCode wRespStatusCode = new HttpStatusCode();
+
+            
+
+            if (uname.Text != "" && pwd.Text != "")
+            {
+                postParameters.Add("username", uname.Text);
+                postParameters.Add("password", pwd.Text);
+                postParameters.Add("strAGB", "AGB");
+                postParameters.Add("strHinweis", "Zahlungsbedingungen");
+
+                postParameters.Add("Login", "");
+
+                string postData = "";
+
+                String url = "https://hotspot.t-mobile.net/wlan/index.do";
+
+                foreach (string key in postParameters.Keys)
+                {
+                    postData += HttpUtility.UrlEncode(key) + "="
+                      + HttpUtility.UrlEncode(postParameters[key]) + "&";
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(postData);
+
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url + "?" + postData);
+                myHttpWebRequest.Method = "GET";
+
+                Console.WriteLine(postData);
+
+                
+                
+                try
+                {
+                    // execute the request
+                    HttpWebResponse response = (HttpWebResponse)
+                        myHttpWebRequest.GetResponse();
+                    // we will read data via the response stream
+                    wRespStatusCode = response.StatusCode;
+                    label1.Text = wRespStatusCode.ToString();
+                    StringBuilder sb = new StringBuilder();
+                    Stream s = response.GetResponseStream();
+                    byte [] buf = new byte[8192];
+                    string tempString = null;
+                    int count = 0;
+                    do
+                    {
+                        count = s.Read(buf, 0, buf.Length);
+                        if (count != 0)
+                        {
+                            tempString = Encoding.ASCII.GetString(buf, 0, count);
+                            sb.Append(tempString);
+                        }
+                    } while (count != 0);
+
+                }
+                catch (WebException)
+                {
+                    label1.Text = "error";
+                }
+            }
+            else
+            {
+                label1.Text = "Please enter credentials";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (uname.Text != "" && pwd.Text != "")
+            {
+                TextWriter tw = new StreamWriter("settings.cnf");
+
+                // write a line of text to the file
+                tw.WriteLine(uname.Text + "|" + pwd.Text);
+
+                // close the stream
+                tw.Close();
+
+                try
+                {
+                    _assembly = Assembly.GetExecutingAssembly();
+                    _imageStream = _assembly.GetManifestResourceStream("HotspotLogin.haken.jpg");
+                    pictureBox1.Image = new Bitmap(_imageStream);
+                }
+                catch
+                {
+                    MessageBox.Show("Error accessing resources!");
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = GetDefaultBrowserPath();
+            p.StartInfo.Arguments = "http://www.pornopurzel.de";
+            p.Start();
+        }
+
+
+        private string GetDefaultBrowserPath()
+        {
+            string key = @"HTTP\shell\open\command";
+            using (RegistryKey registrykey = Registry.ClassesRoot.OpenSubKey(key, false))
+            {
+                return ((string)registrykey.GetValue(null, null)).Split('"')[1];
+            }
+        }
+     
+    }
+}
